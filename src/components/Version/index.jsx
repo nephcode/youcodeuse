0bf94f694c051e350d8c9e6fc60887b6bbbb07f9
Néphélim <nephcohen@gmail.com>
2024-09-15 15:04:33 +0200

    Good job 👌

---
+++ src/components/Version/index.jsx
@@ -0,0 +1,9 @@
+const version = () => {
+  return (
+    <div>
+      <sup>V 2.0.0</sup>
+    </div>
+  );
+};
+export default version;
+// End of Version component ===================================================//
--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,9 +1,10 @@
 import scssHeader from './Header.module.scss';
 
+import version from '../../components/Version';
 const Header = () => {
   return (
     <header>
-      <h1>Nephelim Cohen</h1>
+      <h1 className={scssHeader.headerContent}>Nephelim Cohen</h1>
     </header>
   );
 };
